aacead764a4096b749c1590796b13cf1c2157e8f
miranopari <94349275+miranopari@users.noreply.github.com>
2021-11-23 16:43:46 +0900

    Update uranai.py

--- uranai.py
+++ uranai.py
@@ -255,7 +255,7 @@ def myuranai(input_text):
 
   if 'name' in frame and 'okomari' in frame:
     # 占います
-    number = hash(frame['okomari'])
+    number = str(frame['okomari'])
     if number =='開発職' or number =='研究職':
       return '大学院に行こう！'
     return number
